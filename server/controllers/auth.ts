1d7cc53003bbd9b0f9b42e660c0ff18777577264
Abdulrahman Saber <abdsaber000@gmail.com>
2023-12-06 18:49:03 +0200

    Add login controller

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -19,3 +19,23 @@ export const register =  async (req : Request, res : Response)=>{
     }
     return res.status(403).send("error");
 }
+
+export const login = async (req : Request , res : Response) => {
+    try{
+        const {email , password} = req.body;
+        if(!email || !password){
+            return res.status(404).json({error : "email or password is missing."});
+        }
+        const user = await User.findOne({email : email});
+        if(!user){
+            return res.status(404).json({error : "user not found."});
+        }
+        let isCorrectPassword = await bcrypt.compare(password,  user.password as string);
+        if(!isCorrectPassword){
+            return res.status(404).json({error : "passowrd is not correct"});
+        }
+    }catch(error){
+        console.log(error);
+    }
+    return res.status(200).send("logged successfully");
+}
